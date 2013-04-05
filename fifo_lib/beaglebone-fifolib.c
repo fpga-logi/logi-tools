87e4cf477f0c98b1b653d223d0e9fc1c6e1a64f9
Jonathan Piat <jpiat@yaya.laas.fr>
2013-04-05 10:06:12 +0200

    Adding fifo library to the tools, only beaglebone supported for now

---
+++ fifo_lib/beaglebone-fifolib.c
@@ -0,0 +1,168 @@
+#include "fifolib.h"
+
+
+
+int fd ;
+unsigned int fifo_size ;
+int memory_fd;
+volatile unsigned short * gpmc_pointer ;
+
+struct _fifo fifo_array [MAX_FIFO_NB] ;
+
+int direct_memory_access_init(){
+	int page_size ;	
+	int nb_page ;
+	memory_fd = open("/dev/mem", O_RDWR | O_SYNC);
+	if(memory_fd == -1){
+		printf("error opening /dev/mem \n");
+		exit(EXIT_FAILURE);
+	}
+
+	page_size = getpagesize();
+	nb_page = 0x1FFFE/page_size ;
+	gpmc_pointer = (volatile unsigned short *) mmap(0, nb_page*page_size, 
+	PROT_READ | 
+	PROT_WRITE, 
+	MAP_SHARED ,memory_fd, 
+	FPGA_BASE_ADDR);
+	if(((long) gpmc_pointer) < 0){
+		printf("cannot allocate pointer on %x \n", FPGA_BASE_ADDR);
+		return -1 ;
+	}
+	return 1 ;
+}
+
+void direct_memory_access_close(){
+	close(memory_fd);
+}
+
+
+int fifo_open(unsigned char id){
+	if(id >= MAX_FIFO_NB) return -1 ;
+	fifo_array[id].id = id ;
+	fifo_array[id].offset = id * FIFO_SPACING ;
+	fifo_array[id].open = -1 ;
+	int fd = open("/dev/logibone0", O_RDWR | O_SYNC);
+	if(fd == -1){
+		int ret ;		
+		printf("error opening /dev/logibone0 \n");
+		printf("switching to user space fifo (slowest) \n");
+		if(memory_fd == 0){
+			ret = direct_memory_access_init();
+		}else{
+			ret = 1 ;		
+		}
+		if(ret > 0){
+			 fifo_array[id].size = fifo_getSize(id) ;
+			 fifo_array[id].open = 1 ;
+			 //printf("fifo size is : %d \n",fifo_array[id].size);
+		}
+		return ret ;
+	}
+	ioctl(fd, LOGIBONE_FIFO_MODE) ;
+	return 1 ;
+}
+
+void fifo_close(unsigned char id){
+	if(fd > 0){
+		close(fd);
+	}
+	fifo_array[id].open = -1 ;
+	if(id == 0){
+		direct_memory_access_close();
+	}
+}
+
+int fifo_write(unsigned char id, char * data, unsigned int count){
+	if(fd > 0){
+		ioctl(fd, LOGIBONE_FIFO_MODE);
+		return write(fd, data, count);	
+	}
+	unsigned int transferred = 0 ;
+	unsigned int transfer_size = 0 ;
+	char * src_addr =(char *) data;
+	if(count < FIFO_BLOCK_SIZE){
+		transfer_size = count ;
+	}else{
+		transfer_size = FIFO_BLOCK_SIZE ;
+	}
+	while(transferred < count){
+		while(fifo_getNbFree(id) < transfer_size); 
+		memcpy((void*) &gpmc_pointer[fifo_array[id].offset], src_addr ,transfer_size);
+		src_addr += transfer_size ;
+		transferred += transfer_size ;
+		if((count - transferred) < FIFO_BLOCK_SIZE){
+			transfer_size = count - transferred ;
+		}else{
+			transfer_size = FIFO_BLOCK_SIZE ;
+		}
+	}
+	return transferred ;
+}
+
+int fifo_read(unsigned char id, char * data, unsigned int count){
+	if(fd > 0){
+		ioctl(fd, LOGIBONE_FIFO_MODE);
+		return read(fd, data, count);	
+	}
+	unsigned int transferred = 0 ;
+	unsigned int transfer_size = 0 ;
+	char * trgt_addr =(char *) data;
+	if(count < FIFO_BLOCK_SIZE){
+		transfer_size = count ;
+	}else{
+		transfer_size = FIFO_BLOCK_SIZE ;
+	}	
+	while(transferred < count){
+		while(fifo_getNbAvailable(id) < transfer_size); 
+		memcpy(trgt_addr,(void*) &gpmc_pointer[fifo_array[id].offset], transfer_size); 
+		trgt_addr += transfer_size ;
+		transferred += transfer_size ;
+		if((count - transferred) < FIFO_BLOCK_SIZE){
+			transfer_size = (count - transferred) ;
+		}else{
+			transfer_size = FIFO_BLOCK_SIZE ;
+		}
+	}
+	return transferred ;
+}
+
+void fifo_reset(unsigned char id){
+	
+	if(fd > 0){
+		ioctl(fd, LOGIBONE_FIFO_RESET);
+	}
+	gpmc_pointer[fifo_array[id].offset + FIFO_NB_AVAILABLE_A_OFFSET] = 0 ;
+	gpmc_pointer[fifo_array[id].offset + FIFO_NB_AVAILABLE_B_OFFSET] = 0 ;
+}
+
+unsigned int fifo_getSize(unsigned char id){
+	return ( gpmc_pointer[fifo_array[id].offset + FIFO_SIZE_OFFSET] * 2 );
+}
+
+unsigned int fifo_getNbFree(unsigned char id){
+	if(fd > 0){
+ 		return ioctl(fd, LOGIBONE_FIFO_NB_FREE);
+	}
+	return (fifo_array[id].size - gpmc_pointer[fifo_array[id].offset + FIFO_NB_AVAILABLE_A_OFFSET])*2 ;
+}
+
+
+unsigned int fifo_getNbAvailable(unsigned char id){
+
+	if(fd > 0){
+		return ioctl(fd, LOGIBONE_FIFO_NB_AVAILABLE);
+	}
+	return (gpmc_pointer[fifo_array[id].offset + FIFO_NB_AVAILABLE_B_OFFSET]*2) ;
+}
+
+unsigned int direct_write(unsigned int offset, unsigned char * buffer, unsigned int length){
+	memcpy((void*) &gpmc_pointer[offset/2], buffer, length);
+	return length ;
+}
+unsigned int direct_read(unsigned int offset, unsigned char * buffer, unsigned int length){
+	memcpy(buffer, (void*)&gpmc_pointer[offset/2], length);
+	return length ;
+}
+
+
---
+++ fifo_lib/fifolib.h
@@ -0,0 +1,58 @@
+#include <unistd.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <sys/mman.h>
+#include <string.h>
+#include <fcntl.h>
+#include <errno.h>
+//#include <linux/ioctl.h>
+#include <sys/ioctl.h>
+
+
+#define LOGIBONE_FIFO_IOC_MAGIC 'p'
+#define LOGIBONE_FIFO_RESET _IO(LOGIBONE_FIFO_IOC_MAGIC, 0)
+#define LOGIBONE_FIFO_PEEK _IOR(LOGIBONE_FIFO_IOC_MAGIC, 1, short)
+#define LOGIBONE_FIFO_NB_FREE _IOR(LOGIBONE_FIFO_IOC_MAGIC, 2, short)
+#define LOGIBONE_FIFO_NB_AVAILABLE _IOR(LOGIBONE_FIFO_IOC_MAGIC, 3, short)
+#define LOGIBONE_FIFO_MODE _IO(LOGIBONE_FIFO_IOC_MAGIC, 4)
+#define LOGIBONE_DIRECT_MODE _IO(LOGIBONE_FIFO_IOC_MAGIC, 5)
+
+
+#define FPGA_BASE_ADDR	 0x09000000
+#define FIFO_BASE_ADDR	 0x00
+#define FIFO_CMD_OFFSET  512
+#define FIFO_SIZE_OFFSET	(FIFO_CMD_OFFSET)
+#define FIFO_NB_AVAILABLE_A_OFFSET	(FIFO_CMD_OFFSET + 1)
+#define FIFO_NB_AVAILABLE_B_OFFSET	(FIFO_CMD_OFFSET + 2)
+#define FIFO_PEEK_OFFSET	(FIFO_CMD_OFFSET + 3)
+#define FIFO_READ_OFFSET	0
+#define FIFO_WRITE_OFFSET	0
+#define FIFO_BLOCK_SIZE	1024  //512 * 16 bits
+#define FIFO_SPACING 1024
+
+#define MAX_FIFO_NB 5
+
+struct _fifo{
+	unsigned char id;
+	char open ;
+	unsigned int offset;
+	unsigned int size ;
+};
+
+
+int fifo_open(unsigned char id);
+void fifo_close(unsigned char id);
+int fifo_write(unsigned char id, char * data, unsigned int count);
+int fifo_read(unsigned char id, char * data, unsigned int count);
+unsigned int fifo_getSize(unsigned char id);
+void fifo_reset(unsigned char id);
+unsigned int fifo_getNbFree(unsigned char id);
+unsigned int fifo_getNbAvailable(unsigned char id);
+unsigned int direct_write(unsigned int offset, unsigned char * buffer, unsigned int length);
+unsigned int direct_read(unsigned int offset, unsigned char * buffer, unsigned int length);
+
+
+
+
