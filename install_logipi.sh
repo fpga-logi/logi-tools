cc44fb1b52f51e236612500855bb0dd05bfb973d
Jonathan Piat <jpiat@valentfx.com>
2014-07-07 00:34:20 +0000

    changing some stuff to get ease installation

--- install_logipi.sh
+++ install_logipi.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+SCRIPT_DIR=`pwd`
+
+sudo apt-get update
+
 echo "Enable SPI"
 sed -i s,blacklist spi-bcm2708,#blacklist spi-bcm2708,g /etc/modprobe.d/raspi-blacklist.conf
 modprobe spi-bcm2708
@@ -9,16 +13,23 @@ echo "Install dependencies"
 sudo apt-get install gcc make git
 
 echo "Install logi_loader"
-git clone https://github.com/fpga-logi-dev/logi-tools.git
-cd logi-tools/logipi_loader
+cd logipi_loader
 make
 sudo make install
+cd ${SCRIPT_DIR}
 
 echo "Install python support"
 sudo apt-get install python-dev
 cd logi-tools/python/logipi
 python2 setup.py build
 sudo python2 setup.py install
+cd ${SCRIPT_DIR}
+
+echo "Install logi C library"
+cd c
+sudo make install_logipi
+
 
 echo "Running Blink Led demo"
 logi_loader blink_led.bit
+
