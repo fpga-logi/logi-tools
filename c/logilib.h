75793038f0c44569d038d9b742e75e7a27c1e88d
Jonathan Piat <piat.jonathan@gmail.com>
2014-07-09 12:25:01 +0200

    small modification to allow inclusion of logilib in C++ code

--- c/logilib.h
+++ c/logilib.h
@@ -1,11 +1,19 @@
 #ifndef LOGILIB_H
 #define LOGILIB_H
 
+
+#ifdef __cplusplus
+extern "C" {
+#endif
 int logi_open(void);
 void logi_close(void);
 
 int logi_write(unsigned char * buffer, unsigned int length, unsigned int address);
 int logi_read(unsigned char * buffer, unsigned int length, unsigned int address);
+#ifdef __cplusplus
+}
+#endif
+
 
 #endif
 
