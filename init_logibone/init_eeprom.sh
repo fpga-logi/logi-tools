82f0a9228ac2ff0e4e7b1cd8134ea03f077d46cc
Jonathan Piat <piat.jonathan@gmail.com>
2016-08-31 14:32:27 +0200

    Because of change on i2c bus numbering, script had to be modified.

--- init_logibone/init_eeprom.sh
+++ init_logibone/init_eeprom.sh
@@ -2,4 +2,4 @@
 
 sudo i2cset -y 1 0x24 0x03 0xBF
 sudo i2cset -y 1 0x24 0x01 0x00
-cat data.eeprom > /sys/bus/i2c/drivers/at24/1-0054/eeprom
+cat data.eeprom > /sys/bus/i2c/drivers/at24/2-0054/eeprom
